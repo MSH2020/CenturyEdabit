243d150fd48b78fc76e4307b087295a77aecb14d
MSH2020 <mishaydullin@gmail.com>
2020-03-29 23:03:04 -0600

    Task done

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,9 @@ var year = 1756;
 
 function century(year) {
     var yearString = year.toString(); 
-    var firsTwo = yearString.substring(0, 1);
-    var cent = firstTwo.parseInt() + 1;
-    
-    var resultDisplayed = year + " is in " + cent + " century";
+    var firstTwo = yearString.substring(0, 2);
+    var cent = parseInt(firstTwo) + 1;
+    var resultDisplayed = year + " is in " + cent + "th century";
     console.log ( resultDisplayed );
     return cent;
 }
